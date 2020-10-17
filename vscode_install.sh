9af88cef6b100ff2f8c88b21b8bbbe5a1143bb97
Christopher Barnes <cbarnes@tmsl.com>
2020-10-17 21:16:43 +0100

    Create vscode_install.sh

---
+++ vscode_install.sh
@@ -0,0 +1,14 @@
+sudo apt install -y build-essential make cmake cmake-curses-gui git g++ pkg-config curl libfreetype6-dev libcanberra-gtk-module libcanberra-gtk3-module python3-dev python3-testresources python3-pip 
+sudo apt install -y git libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm libnss3 apt-transport-https 
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - 
+sudo apt install -y nodejs 
+sudo apt install gcc g++ make 
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 
+sudo apt update 
+sudo apt install yarn 
+yarn --version 
+cd ~/ 
+git clone https://github.com/microsoft/vscode 
+cd vscode 
+yarn 
+yarn run watch 
