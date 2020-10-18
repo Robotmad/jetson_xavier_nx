2d330a0e2edc4b9f141c39573a83ecb564218b6b
Christopher Barnes <cbarnes@tmsl.com>
2020-10-18 11:18:32 +0100

    extras

---
+++ extras_install.sh
@@ -0,0 +1,33 @@
+#Virtual Environemtns
+sudo apt install virtual env
+#python3 -m virtualenv -p python3 <chosen_venv_name>
+python3 -m virtualenv -p python3 main
+
+# source <chosen_venv_name>/bin/activate
+source main/bin/activate
+
+sudo apt install -y build-essential make cmake cmake-curses-gui   git g++ pkg-config curl libfreetype6-dev libcanberra-gtk-module libcanberra-gtk3-module   python3-dev python3-testresources python3-pip
+sudo pip3 install -U pip Cython
+sudo pip3 install numpy matplotlib
+
+git clone https://github.com/jkjung-avt/jetson_nano.git jkjung
+cd jkjung
+./install_protobuf-3.8.0.sh
+
+# jetsonStats - "jtop"
+sudo –H pip3 install –U jetson-stats
+
+# Tensort2RT
+
+#JetCam - may be using old OpenCV?
+cd ~/
+git clone git@github.com:NVIDIA-AI-IOT/jetcam
+cd jetcam
+sudo python3 setup.py install
+
+
+#Torchvision
+
+# Robot Repo
+git clone git@github.com:Robotmad/Robot
+
